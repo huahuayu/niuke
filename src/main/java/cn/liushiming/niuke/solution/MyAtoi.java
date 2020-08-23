c1d61a4e2718df86af6a1c817cc69f66d0ebc418
Shiming <codingmylife@gmail.com>
2020-08-23 13:34:13 +0800

    增加题目

--- src/main/java/cn/liushiming/niuke/solution/MyAtoi.java
+++ src/main/java/cn/liushiming/niuke/solution/MyAtoi.java
@@ -1,5 +1,7 @@
 package cn.liushiming.niuke.solution;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,7 +9,7 @@ import java.util.Scanner;
  * @Author shiming
  * @Date 2020/8/19 9:47 PM
  * @Version 1.0
- * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+ * 请你来实现一个atoi函数，使其能将字符串转换成整数。
  * <p>
  * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。接下来的转化规则如下：
  * <p>
@@ -57,28 +59,23 @@ public class MyAtoi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        System.out.println(myAtoi(str));
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
+        System.out.println(myAtoi1(str));
+        System.out.println(myAtoi2(str));
     }
 
-    public static int myAtoi(String str) {
-        if (str == null) {
-            return 0;
-        }
-        str = str.trim();
-        if ("".equals(str)) {
+    public static int myAtoi1(String str) {
+        if (str == null || "".equals(str.trim())) {
             return 0;
         }
 
         int res = 0;
         int i = 0;
-        int flag = 1;
+        int sign = 1;
         while (str.charAt(i) == ' ') {
             i++;
         }
         if (str.charAt(i) == '-') {
-            flag = -1;
+            sign = -1;
         }
         if (str.charAt(i) == '+' || str.charAt(i) == '-') {
             i++;
@@ -90,12 +87,70 @@ public class MyAtoi {
                 break;
             }
             if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && cur > Integer.MAX_VALUE % 10)) {
-                return flag > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             res = res * 10 + cur;
             i++;
         }
 
-        return res * flag;
+        return res * sign;
+    }
+
+    public static int myAtoi2(String str) {
+        Automaton automaton = new Automaton();
+        for (int i = 0; i < str.length(); i++) {
+            automaton.get(str.charAt(i));
+            if (Automaton.END.equals(automaton.state)) {
+                break;
+            }
+        }
+        return automaton.sign * ((int) automaton.ans);
+    }
+
+    static class Automaton {
+        private static final String START = "start";
+        private static final String SIGNED = "signed";
+        private static final String IN_NUM = "in_number";
+        private static final String END = "end";
+        private String state = START;
+        private Map<String, String[]> map;
+        private int sign = 1;
+        private long ans = 0;
+
+        public Automaton() {
+            map = new HashMap<>();
+            map.put(START, new String[]{START, SIGNED, IN_NUM, END});
+            map.put(SIGNED, new String[]{END, END, IN_NUM, END});
+            map.put(IN_NUM, new String[]{END, END, IN_NUM, END});
+            map.put(END, new String[]{END, END, END, END});
+        }
+
+        public int get_col(char c) {
+            if (c == ' ') {
+                return 0;
+            }
+            if (c == '+' || c == '-') {
+                return 1;
+            }
+            if (c >= '0' && c <= '9') {
+                return 2;
+            }
+            return 3;
+        }
+
+        public void get(char c) {
+            state = map.get(state)[get_col(c)];
+            if (state.equals(IN_NUM)) {
+                ans = ans * 10 + c - '0';
+                if (sign == 1) {
+                    ans = Math.min(ans, Integer.MAX_VALUE);
+                } else {
+                    // -(long)Integer.MIN_VALUE，这个操作有点东西，不然越界
+                    ans = Math.min(ans, -(long) Integer.MIN_VALUE);
+                }
+            } else if (state.equals(SIGNED)) {
+                sign = c == '+' ? 1 : -1;
+            }
+        }
     }
 }
